358e013b774df503a00733eee8f8c2bb7ef27d38
Ryszard P <Riczm0ndes@gmail.com>
2022-04-20 22:00:07 +0200

    Add products to Cart from Product Details View

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -25,13 +25,7 @@ export class CartService {
 
     if (this.cartItems.length > 0) {
       //find the item cart based on item id
-      for (let tempCartItem of this.cartItems) {
-        if (tempCartItem.id === theCartItem.id) {
-          existingCartItem = tempCartItem;
-          break;
-        }
-      }
-
+      existingCartItem = this.cartItems.find(tempCarItem => tempCarItem.id === theCartItem.id)!;
       //  check if we found it
       alreadyExistsInCart = (existingCartItem != undefined);
     }
